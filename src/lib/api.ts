b9ee746af43bbf5a6d9a19dcf7e68dde60dbf5e7
cgloveut <pitsanubomb@gmail.com>
2021-10-08 16:47:57 +0700

    :fire: Add group sun_behind_small_cloud

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,12 +3,15 @@ export default {
     const API = `https://api-telegramadmin.herokuapp.com/${pathUri}`;
     try {
       return await fetch(API).then((response) => response.json());
-    } catch (error: any) {
-      throw error.message || `Not Found`;
+    } catch (error) {
+      throw error || `Not Found`;
     }
   },
 
   async getUser() {
     return await this.get(`user`);
+  },
+  async getGroup() {
+    return await this.get(`group`);
   },
 };
